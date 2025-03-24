ed1b2ea80f5ef7c65f5d854e8ee510593cdb4319
AayushiDudhat <aayushidudhat2004@gmail.com>
2025-03-24 22:43:20 +0530

    dynamic product added

--- pickle_pro/userprofile/views.py
+++ pickle_pro/userprofile/views.py
@@ -1,43 +1,36 @@
-from django.shortcuts import render,redirect,get_object_or_404
+from django.shortcuts import render,redirect
 from .models import *
 from django.contrib import messages
 from django.contrib.auth import authenticate,login as auth_login,logout
+from django.contrib.auth import logout
 import ipdb
 
 def home(request):
-    if request.method == 'GET':
-        queryset = Category.objects.all()
-        context ={
-            'queryset' : queryset,
-        }
-        return render(request,'home.html', context)
     return render(request,'home.html')
 
-def product_detail(request,id):
-    category = Category.objects.get(id = id)
-    products = Product.objects.filter(Category=category)
-    context ={
-        "category" : category,
-        "products" : products,
-    }
-
 def mango(request):
-    return render(request,'mango.html')
+    mango_products = MangoProduct.objects.all()
+    return render(request, 'mango.html', {'mango_products': mango_products})
 
 def Lemon(request):
-    return render(request,'Lemon.html')
+    lemon_products = LemonProduct.objects.all()
+    return render(request,'Lemon.html', {'lemon_products': lemon_products})
 
 def Mixed(request):
-    return render(request,'Mixed.html')
+    mixed_products = MixedProduct.objects.all()
+    return render(request,'Mixed.html', {'mixed_products': mixed_products})
 
 def Panjabi(request):
-    return render(request,'Panjabi.html')
+    panjabi_products = PanjabiProduct.objects.all()
+    return render(request,'Panjabi.html', {'panjabi_products': panjabi_products})
 
 def kerda(request):
-    return render(request,'kerda.html')
+    kerda_products = KerdaProduct.objects.all()
+    return render(request,'kerda.html', {'kerda_products': kerda_products})
 
 def Carrot(request):
-    return render(request,'Carrot.html')
+    mango_products = CarrotProduct.objects.all()
+    return render(request,'Carrot.html', {'mango_products': mango_products})
 
 def spicy(request):
     return render(request,'spicy.html')
@@ -82,7 +75,7 @@ def password(request):
     return render(request,'password.html')
 
 def login(request):
-    if request.method == 'POST':  # Ensure it only processes POST requests
+    if request.method == 'POST':  
         email = request.POST['email']
         print(email)
         password = request.POST['password']
@@ -110,42 +103,8 @@ def login(request):
 
 def logout_view(request):
     logout(request)
-    return render(request,'home.html')
+    return redirect('home')  
 
 def checkout(request):
     return render(request,'checkout.html')
 
-# def view_cart(request):
-#     user = request.user
-#     cart = Cart.objects.get(user=user)
-#     cart_items = cart.items.all()
-#     cart_count = cart_items.count()
-    
-#     context= {
-#         'cart': cart,
-#         'cart_items': cart_items,
-#         'cart_count': cart_count
-#     }
-#     return render(request,'cart.html',context)
-
-# def add_to_cart(request, product_id):
-#     user = request.user
-#     if not user.is_authenticated:
-#         messages.error(request, 'You must be logged in to add items to your cart.')
-#         return redirect('login')
-
-#     product = Product.objects.get(id=product_id)
-#     cart, created = Cart.objects.get_or_create(user= request.user)
-#     cart_item, created=CartItem.objects.get_or_create(cart=cart, product=product)
-
-    
-#     if not created:
-#         cart_item.quantity +=1
-
-#     cart_item.save()
-#     return redirect('view_cart')
-
-# def remove_from_cart(request, item_id):
-#     cart_item = get_object_or_404( CartItem, id=item_id, cart__user=request.user)
-#     cart_item.delete()
-#     return redirect('view_cart')
